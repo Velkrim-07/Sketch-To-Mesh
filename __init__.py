121b96e41c59f23d83fd7383523bc6778bb69678
Judah Smith-dyer <jmsmithdyer@student.fullsail.edu>
2024-02-06 16:30:58 -0500

    Connected Cv2 to the Project
    the program will now input the front image

--- __init__.py
+++ __init__.py
@@ -7,100 +7,105 @@ bl_info = {
     "description": "The Inital UI skeleton",
     "category": "Development",
 }
-
     # 3D Viewport area (find list of values here https://docs.blender.org/api/current/bpy_types_enum_items/space_type_items.html#rna-enum-space-type-items)
     # Sidebar region (find list of values here https://docs.blender.org/api/current/bpy_types_enum_items/region_type_items.html#rna-enum-region-type-items)
 
 import bpy
+import cv2
+import numpy as np
+import os
+
 from .db_operations import test_connection
-from typing import Set
-from bpy.types import Context
+#this import is not used ye
+# from typing import Set 
+
+
+def outline_image(image_path, Extension, ImgName, Filedirectory):
+    """Read an image from a path, outline it, calculate the center of mass for the outlines, and draw a blue dot there."""
+    image = cv2.imread(image_path)
+    if image is None:
+        print("Error: Image not found or unable to load.")
+        return
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Check if any contours were found
+    if contours:
+        # Draw contours on the original image
+        cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+
+        # Calculate the combined center of mass for all contours
+        totalX, totalY, totalArea = 0, 0, 0
+        for contour in contours:
+            M = cv2.moments(contour)
+            if M["m00"] != 0:
+                totalX += int(M["m10"])
+                totalY += int(M["m01"])
+                totalArea += M["m00"]
+        if totalArea != 0:
+            cX = int(totalX / totalArea)
+            cY = int(totalY / totalArea)
+            # Draw a blue dot at the combined center of mass
+            cv2.circle(image, (cX, cY), 5, (255, 0, 0), -1)
+        else:
+            print("Error: Combined center of mass could not be calculated.")
+    else:
+        print("Error: No contours found.")
+
+    os.chdir(Filedirectory) #changes the directory to the folder where we are going to save the file
+    cv2.imwrite(ImgName + Extension, image) #saves the image
+    os.chdir("..\\" + Filedirectory) #goes back one directory
 
 
 
-class Reset_Center_Axis_Operator(bpy.types.Operator):
-    bl_idname = "object.reset_center_axis"
+#Image to plane code
+#       bpy.ops.import_image.to_plane(files=[{"name":"A'shla1.jpg", "name":"A'shla1.jpg"}], directory="C:\\Users\\judah\\Pictures\\Art\\A'shala\\", relative=False)
+#
+
+class PlaceImageIn3D(bpy.types.Operator):
+    bl_idname = "object.place_image_in_space"
     bl_label ="Reset"
 
     def execute(self, context):
-        self.active_X_Center_Object = bpy.data.objects.get("Image_Center_X")
-        self.active_Y_Center_Object = bpy.data.objects.get("Image_Center_Y")
+        My_file_path = bpy.path.abspath(bpy.context.scene.front_views_file_path) #this converts the file fath in something we can actually use
+        NewFilePath: bpy.props.StringProperty(subtype="FILE_PATH")
 
-        midpoint = 50
+        if My_file_path:
+            LastPeriod = My_file_path.rfind(".")
+            Extension =  My_file_path[LastPeriod: ]
 
-        self.active_X_Center_Object = midpoint
-        self.active_Y_Center_Object = midpoint
+            ImageDirection = "frontImage"
+            ImageDiretory = "ImageFolder\\"
+            #this is where we want to save the picture so i can be reaccessed
+            outline_image(My_file_path, Extension, ImageDirection, ImageDiretory)
+            NewFilePath = os.path.abspath( ImageDiretory + ImageDirection + Extension)
+
+        if NewFilePath:
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.paint.texture_paint_toggle()
+            bpy.ops.paint.texture_paint_toggle()
+            bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+            bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+
+            
+
+            Lastslash = NewFilePath.rfind("\\")
+            
+            filename = os.path.basename(NewFilePath)
+            FileDirectory = NewFilePath[:Lastslash] + "\\"
+            
+            #bpy.ops.import_image.to_plane(files=[{"name":filename, "name":filename}], directory=FileDirectory, relative=False)
+
+            bpy.ops.import_image.to_plane(files=[{"name":'frontImage'+Extension, "name":'frontImage'+Extension}], directory="C:\\Users\\judah\\Desktop\\Code Stuffs\\Filtered2\\Filtered\\Sketch-To-Mesh\\ImageFolder\\", relative=False)
+        else:
+            self.report({'ERROR'}, "No inputted Image.")
 
         return {'FINISHED'}
-
-
-
-class DoImg(bpy.types.Operator):
-    bl_idname = "object.do_img"
-    bl_label = "Place Image"
     
-    #Property that holds the filepath that will be used to insert the image
-    myFilePath: bpy.props.StringProperty(subtype="FILE_PATH")
-
-    #This is the function that inserts the image into blender
-    def execute(self, context):
-        file_path = self.myFilePath
-                
-        if file_path: # Checks if the file path is not empty
-            bpy.ops.image.open(filepath=file_path) # opens the image file
-            loaded_image = bpy.data.images.get(bpy.path.basename(file_path))
-
-            if loaded_image:
-                # Display the image in the Image Editor
-                bpy.context.area.type = 'IMAGE_EDITOR'
-                bpy.context.space_data.image = loaded_image
-        
-        return {'FINISHED'}
     
-    #This is a function that opens a file explorer
-    #THIS DOES NOT DO ANYTHING I just think its going to be useful to have later
-    def invoke(self, context, event):
-        # Open a file browser to select a file
-        context.window_manager.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-    
-
-
-class Create_Alignment_Window(bpy.types.Operator):
-    bl_idname = "load.open_align_image" 
-    bl_label = "Align Image"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    #Property that holds the filepath that will be used to insert the image
-    FilePath: bpy.props.StringProperty(subtype="FILE_PATH")
-
-    #this is where you define what you want to happen
-    def execute(self, context):
-        layout = self.layout
-        self.FilePath = context.scene.front_views_file_path.filepath
-        return {'FINISHED'}
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, "FilePath")
-
-        row = layout.row()
-        if self.FilePath != FileNotFoundError:  row.operator("object.do_img", text="Align Image").myFilePath = self.FilePath
-
-        row = layout.row()
-        row.prop(context.scene, "Image_Center_X", text="Image X center", slider=True)
-
-        row = layout.row()
-        row.prop(context.scene, "Image_Center_Y", text="Image Y Center", slider=True)
-
-        row = layout.row()
-        row.operator("object.reset_center_axis", text="Reset").__class__
-
-
-    def invoke(self, context, event):
-        return context.window_manager.invoke_props_dialog(self)
-    
-
 
 # this contains the main layout for the Sketch to mesh program
  # right now the program will not really do anything. 
@@ -112,7 +117,7 @@ class Create_Alignment_Window(bpy.types.Operator):
 class VIEW3D_PT_Sketch_To_Mesh_Panel(bpy.types.Panel):  
     bl_space_type = "VIEW_3D"  
     bl_region_type = "UI"  
-    bl_idname = "PT_Sketch_To_Mesh_Main_Panel"
+    bl_idname = "_PT_Sketch_To_Mesh_Main_Panel" 
 
     bl_category = "S-T-M"  # Sidebar cName
     bl_label = "Sketch-To-Mesh"  # found at the top of the Panel
@@ -139,8 +144,8 @@ class StMTestConnectionOperator(bpy.types.Operator):
 
 class VIEW3D_PT_Sketch_To_Mesh_Views_Panel(bpy.types.Panel):  
     bl_label = "View"
-    bl_idname = "PT_Views"
-    bl_parent_id = "PT_Sketch_To_Mesh_Main_Panel" 
+    bl_idname = "_PT_Views"
+    bl_parent_id = "_PT_Sketch_To_Mesh_Main_Panel" 
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
@@ -161,25 +166,46 @@ class VIEW3D_PT_Sketch_To_Mesh_Views_Panel(bpy.types.Panel):
 
 
 
+class DoImg(bpy.types.Operator):
+    bl_idname = "object.do_img"
+    bl_label = "Place Image"
+
+    #Property that holds the filepath that will be used to insert the image
+    myFilePath: bpy.props.StringProperty(subtype="FILE_PATH")
+
+    #This is the function that inserts the image into blender
+    def execute(self, context):
+        bpy.ops.object.load_reference_image(filepath=self.myFilePath)
+        return {'FINISHED'}
+    
+    #This is a function that opens a file explorer
+    #THIS DOES NOT DO ANYTHING I just think its going to be useful to have later
+    def invoke(self, context, event):
+        # Open a file browser to select a file
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+
+
 class VIEW3D_PT_Sketch_To_Mesh_Align_Views_Panel(bpy.types.Panel):  
     bl_label = "AlignViews"
-    bl_idname = "PT_AlignViews"
-    bl_parent_id = "PT_Views"
+    bl_idname = "_PT_AlignViews"
+    bl_parent_id = "_PT_Views"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
     def draw(self, context):
         layout = self.layout
-        row = layout.row()
 
-        row.operator("load.open_align_image", text="Align Image")
+        row = layout.row()
+        row.operator("object.place_image_in_space", text="Align Image")
 
        
 
 class VIEW3D_PT_Sketch_To_Mesh_MeshSettings_Panel(bpy.types.Panel):  
     bl_label = "MeshSettings"
-    bl_idname = "PT_MeshSettings"
-    bl_parent_id = "PT_Sketch_To_Mesh_Main_Panel"  # Set the parent panel ID
+    bl_idname = "_PT_MeshSettings"
+    bl_parent_id = "_PT_Sketch_To_Mesh_Main_Panel"  # Set the parent panel ID
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
@@ -241,15 +267,13 @@ def register():
     bpy.types.Scene.side_views_file_path = bpy.props.StringProperty(subtype="FILE_PATH")
     bpy.types.Scene.poly_count_range = bpy.props.IntProperty(name="Poly Count", default=10, min=0, max=100)
     bpy.types.Scene.mesh_rating = bpy.props.IntProperty(name="Mesh Rating", default=10, min=0, max=100)
-    bpy.types.Scene.Image_Center_X = bpy.props.IntProperty(name="Mesh Rating", default=10, min=0, max=100)
-    bpy.types.Scene.Image_Center_Y = bpy.props.IntProperty(name="Mesh Rating", default=10, min=0, max=100)
-    bpy.types.Scene.mesh_rating = bpy.props.IntProperty(name="Mesh Rating", default=10, min=0, max=100)
+    bpy.types.Scene.Image_Center_X = bpy.props.IntProperty(name="Image Center X", default=10, min=0, max=100)
+    bpy.types.Scene.Image_Center_Y = bpy.props.IntProperty(name="Image Center Y", default=10, min=0, max=100)
     bpy.types.Scene.FileName_Input = bpy.props.StringProperty(name="FileName", default="STMFile")
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_Panel)
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_Views_Panel)
-    bpy.utils.register_class(Reset_Center_Axis_Operator)
+    bpy.utils.register_class(PlaceImageIn3D)
     bpy.utils.register_class(DoImg)
-    bpy.utils.register_class(Create_Alignment_Window)
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_Align_Views_Panel) 
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_MeshSettings_Panel)
     # ralf changes
@@ -268,9 +292,8 @@ def unregister():
     del bpy.types.Scene.FileName_Input
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_Panel)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_Views_Panel)
-    bpy.utils.unregister_class(Reset_Center_Axis_Operator)
+    bpy.utils.unregister_class(PlaceImageIn3D)
     bpy.utils.unregister_class(DoImg)
-    bpy.utils.unregister_class(Create_Alignment_Window)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_Align_Views_Panel)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_MeshSettings_Panel)
     # ralf changes
