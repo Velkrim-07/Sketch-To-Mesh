7da4f69dc705f145504e52f1a1c30352f4ecd999
rfs1998 <61716210+rfs1998@users.noreply.github.com>
2024-02-08 03:07:21 -0500

    image_processing has methods that deal with feature matching now. unsure if it will continue there, but for now it is there for testing.
    - orb an AKAZE feature detection with Ratio test to minimze false matches.

--- image_processing.py
+++ image_processing.py
@@ -42,6 +42,79 @@ def prepare_image(image_path):
         return False
     
 
+def match_features(descriptors1, descriptors2, method='ORB'):
+    # using ORB and AKAZE for testing
+    if method == 'ORB':
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    else:  
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+    # knn Matching descriptors
+    matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+    
+    # RATIO TEST
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.5 * n.distance:  # Ratio test
+            good_matches.append(m)
+    
+    return good_matches
+
+def draw_matches(image1, keypoints1, image2, keypoints2, matches):
+
+    matched_image = cv2.drawMatches(image1, keypoints1, image2, keypoints2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    return matched_image
+
+
+def detect_and_describe_akaze(image_path):
+    image = cv2.imread(image_path)
+    
+    if image is None:
+        raise IOError("Could not open the image from path: {}".format(image_path))
+    
+    akaze = cv2.AKAZE_create()
+    keypoints = akaze.detect(image, None)
+    
+    # compute descriptors for the detected keypoints
+    keypoints, descriptors = akaze.compute(image, keypoints)
+    
+    return keypoints, descriptors
+
+def test():
+
+    img_path1 = 'C:/Users/RAFAEL MUITO ZIKA/Desktop/Test/front.png'
+    img_path2 = 'C:/Users/RAFAEL MUITO ZIKA/Desktop/Test/side.png'
+    img_path3 = 'C:/Users/RAFAEL MUITO ZIKA/Desktop/Test/sidee.png' 
+
+    # detect
+    keypoints1, descriptors1 = detect_and_describe_akaze(img_path1)
+    keypoints2, descriptors2 = detect_and_describe_akaze(img_path2)
+    keypoints3, descriptors3 = detect_and_describe_akaze(img_path3)
+
+    # match
+    matches12 = match_features(descriptors1, descriptors2, method='AKAZE')
+    matches23 = match_features(descriptors2, descriptors3, method='AKAZE')
+    matches13 = match_features(descriptors1, descriptors3, method='AKAZE')
+
+    # visualize
+    image1 = cv2.imread(img_path1)
+    image2 = cv2.imread(img_path2)
+    image3 = cv2.imread(img_path3)
+
+    matched_image12 = draw_matches(image1, keypoints1, image2, keypoints2, matches12)
+    matched_image23 = draw_matches(image2, keypoints2, image3, keypoints3, matches23)
+    matched_image13 = draw_matches(image1, keypoints1, image3, keypoints3, matches13)
+
+    # display
+    cv2.imshow('Matches between Image 1 and Image 2', matched_image12)
+    cv2.imshow('Matches between Image 2 and Image 3', matched_image23)
+    cv2.imshow('Matches between Image 1 and Image 3', matched_image13)
+
+    cv2.waitKey(0)  # Wait for a key press to exit
+    cv2.destroyAllWindows()  # Close all the opened windows
+
 # example usage
-# image_path = 'C:/Users/RAFAEL MUITO ZIKA/Pictures/emoji disdcord/pekora fate.png'
-# prepared_image = prepare_image(image_path)
+#image_path = 'C:/Users/RAFAEL MUITO ZIKA/Pictures/emoji disdcord/pekora fate.png'
+#prepared_image = prepare_image(image_path)
+
+
