10d55edc5110e189f31a7f534f06d3845715a819
Judah Smith-dyer <jmsmithdyer@student.fullsail.edu>
2024-02-12 20:15:29 -0500

    STD-56
    This should have probably had it's own branch, but there is now a reset button that resets the scene and allows the user to replace the filepaths

--- __init__.py
+++ __init__.py
@@ -16,12 +16,12 @@ import numpy as np
 import os
 from dataclasses import dataclass
 from .db_operations import test_connection
-#this import is not used ye
+from .image_processing import prepare_image # the . is on purpose. do not remove
+#this import is not used yet
 # from typing import Set 
 
-
-globalPlaneArray = []
-globalfilePathsArray = []
+globalPlaneArray = [] # a list of the names of the planes in Blender
+globalfilePathsArray = [] # a list of the file path we wan to keep track of
 
 #FutureReference
 #@dataclass
@@ -29,55 +29,84 @@ globalfilePathsArray = []
     #filePath: bpy.props.StringProperty(subtype="FILE_PATH")
     #Description: str
 
+    # import cv2
+from .image_processing import prepare_image # the . is on purpose. do not remove
+class StMTestImagePrep(bpy.types.Operator):
+    bl_idname = "wm.prepare_image_operator"
+    bl_label = "Test Image Prep"
+
+    def execute(self, context):
+        
+        path = 'C:/Users/RAFAEL MUITO ZIKA/Desktop/Test/front.png'
+        success = prepare_image(path)
+        if success:
+            self.report({'INFO'}, "Image Prep Succesful!")
+        else:
+            self.report({'ERROR'}, "Failed to Image Prep.")
+
+        return {'FINISHED'}
 
 
 def outline_image(image_path, Extension, ImgName, Filedirectory):
     """Read an image from a path, outline it, calculate the center of mass for the outlines, and draw a blue dot there."""
     image = cv2.imread(image_path)
+
     if image is None:
         print("Error: Image not found or unable to load.")
         return
+    
+    # temporary file size. adjusting files to the same scale can be beneficial for feature detectors
+    resized_image = cv2.resize(image, (800, 600))  
+    # grayscale reduces computational load
+    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY) 
+    # noise reduction
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    # canny edge detection emphasizes edges in the image
+    # we will most likely be using one of the two as feature detectors: ORB, AKAZE.
+    # both feature detection algorithms have positive results from this as they often rely on edge information to find key points.
+    edges = cv2.Canny(blurred_image, 50, 15, apertureSize=3)
+
+    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Check if any contours were found
     if contours:
         # Draw contours on the original image
-        cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+        cv2.drawContours(resized_image, contours, -1, (0, 255, 0), 2)
 
         # Calculate the combined center of mass for all contours
         totalX, totalY, totalArea = 0, 0, 0
+
         for contour in contours:
             M = cv2.moments(contour)
+
             if M["m00"] != 0:
                 totalX += int(M["m10"])
                 totalY += int(M["m01"])
                 totalArea += M["m00"]
+
         if totalArea != 0:
             cX = int(totalX / totalArea)
             cY = int(totalY / totalArea)
             # Draw a blue dot at the combined center of mass
-            cv2.circle(image, (cX, cY), 5, (255, 0, 0), -1)
+            cv2.circle(resized_image, (cX, cY), 5, (255, 0, 0), -1)
+
         else:
             print("Error: Combined center of mass could not be calculated.")
     else:
         print("Error: No contours found.")
         
     os.chdir(Filedirectory) #changes the directory to the folder where we are going to save the file
-    cv2.imwrite(ImgName + Extension, image) #saves the image
+    cv2.imwrite(ImgName + Extension, resized_image) #saves the image
     os.chdir("..\\") #goes back one directory
 
 
 
-#Image to plane code
-#       bpy.ops.import_image.to_plane(files=[{"name":"A'shla1.jpg", "name":"A'shla1.jpg"}], directory="C:\\Users\\judah\\Pictures\\Art\\A'shala\\", relative=False)
-#
-
 class PlaceImageIn3D(bpy.types.Operator):
     bl_idname = "object.place_image_in_space"
-    bl_label ="Reset"
+    bl_label ="Place Images"
 
     PlaneArray = globalPlaneArray
     filePathsArray = globalfilePathsArray
@@ -93,10 +122,7 @@ class PlaceImageIn3D(bpy.types.Operator):
         #this is only here for now
         filePathsArray = [bpy.context.scene.front_views_file_path, bpy.context.scene.back_views_file_path,  bpy.context.scene.side_views_file_path ]
     
-
         for file_path in filePathsArray : 
-            NewFilePath: bpy.props.StringProperty(subtype="FILE_PATH")
-
             if file_path:
                 #this is used for the new image. We want to save the new image as the same type of file as the first
                 Extension =  file_path[file_path.rfind("."): ] 
@@ -109,27 +135,34 @@ class PlaceImageIn3D(bpy.types.Operator):
 
                 #this is the file name for the image we are creating
                 ImgName = "View" + str(Itervalue)
+                self.PlaneArray.insert(Itervalue, ImgName) # allows us to access the plane after creation
 
                 #this is where we want to save the picture so i can be reaccessed
                 outline_image(file_path, Extension, ImgName, ImageDiretoryForNewImage)
+
                 #this creates a new file path to the image we just saved
                 NewFilePath = os.path.abspath(ImageDiretoryForNewImage + "\\" + ImgName + Extension) 
                 
                 if NewFilePath:
-                    bpy.ops.object.editmode_toggle()
-                    bpy.ops.paint.texture_paint_toggle()
-                    bpy.ops.mesh.primitive_plane_add(size=4, enter_editmode=False, align='WORLD', location=(0, 0, 0), rotation=RotationValue, scale=(20, 20, 20))
-                    bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
-                    
                     filename = os.path.basename(NewFilePath)
                     FileDirectory = NewFilePath[: NewFilePath.rfind("\\")] + "\\"
-                    
-                    #bpy.ops.import_image.to_plane(files=[{"name":filename, "name":filename}], directory=FileDirectory, relative=False)
 
-                    plane = bpy.ops.import_image.to_plane(files=[{"name":filename, "name":filename}], directory=FileDirectory, relative=False)
-                    self.planeArray.insert(plane) # allows us to access the plane after creation
+                    #bpy.ops.import_image.to_plane(files=[{"name":filename, "name":filename}], directory=FileDirectory, relative=False)
+                    bpy.ops.import_image.to_plane(files=[{"name":filename, "name":filename}], directory=FileDirectory, relative=False)
+
+                    #we set the rotation and location of each plane
+                    bpy.data.objects[ImgName].select_set(True)
+                    match Itervalue :
+                        case 1: bpy.ops.transform.translate(value=(-0.01, 0 , 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, alt_navigation=True)
+                        case 2:bpy.context.object.rotation_euler[2] = 0
+                    
                 else:
-                    self.report({'ERROR'}, "No inputted Image.")
+                    match Itervalue:
+                        case 0: MissingView = "FontView"
+                        case 1: MissingView = "BackView"
+                        case 2: MissingView = "SideView"
+
+                    self.report({'ERROR'}, "No inputted Image for" + MissingView)
                 Itervalue = Itervalue + 1
             else:
                 self.report({'ERROR'}, "No inputted Image.")
@@ -137,7 +170,8 @@ class PlaceImageIn3D(bpy.types.Operator):
 
         return {'FINISHED'}
     
-    
+
+   
 
 # this contains the main layout for the Sketch to mesh program
  # right now the program will not really do anything. 
@@ -164,8 +198,7 @@ class StMTestConnectionOperator(bpy.types.Operator):
     bl_idname = "wm.test_connection_operator"
     bl_label = "Test Database Connection"
 
-    def execute(self, context):
-        
+    def execute(self, context): 
         success = test_connection()
         if success:
             self.report({'INFO'}, "Connection to MongoDB successful!")
@@ -173,22 +206,36 @@ class StMTestConnectionOperator(bpy.types.Operator):
             self.report({'ERROR'}, "Failed to connect to MongoDB.")
         return {'FINISHED'}
 
-# import cv2
-from .image_processing import prepare_image # the . is on purpose. do not remove
-class StMTestImagePrep(bpy.types.Operator):
-    bl_idname = "wm.prepare_image_operator"
-    bl_label = "Test Image Prep"
+
+
+class Reset_Input_Images(bpy.types.Operator): 
+    bl_idname = "object.reset_selected_images"
+    bl_label = "Reset_Images"
+    
+    myFilePathArray = globalfilePathsArray # saves the entries in the global list to be deleted
+    ImagePanelArray = globalPlaneArray
+    globalPlaneArray.clear()
+    globalfilePathsArray.clear() # clears the global list 
 
     def execute(self, context):
-        
-        path = 'C:/Users/RAFAEL MUITO ZIKA/Desktop/Test/front.png'
-        success = prepare_image(path)
-        if success:
-            self.report({'INFO'}, "Image Prep Succesful!")
-        else:
-            self.report({'ERROR'}, "Failed to Image Prep.")
+        Itervalue = 0
+
+        for filePath in self.myFilePathArray :
+            #Finds the Images Saved
+            ImageFilePath = os.path.abspath("ImageFolder\\" + "View" + str(Itervalue) + filePath[filePath.rfind("."): ] ) 
+
+            # if we find that file we will delete it
+            if ImageFilePath:
+                os.remove(ImageFilePath)
+
+        for images in self.ImagePanelArray:
+            # selects the image plane in the array 
+            bpy.data.objects[images].select_set(True)
+            #deletes the image plane in the array
+            bpy.ops.object.delete(use_global=False, confirm=False)
 
         return {'FINISHED'}
+
      
 
 
@@ -205,7 +252,6 @@ class VIEW3D_PT_Sketch_To_Mesh_Views_Panel(bpy.types.Panel):
         row = layout.row()
         row.prop(context.scene, "front_views_file_path", text="Front View")
        
-
         row = layout.row()
         row.prop(context.scene, "back_views_file_path", text="Back View")
 
@@ -213,7 +259,13 @@ class VIEW3D_PT_Sketch_To_Mesh_Views_Panel(bpy.types.Panel):
         row.prop(context.scene, "side_views_file_path", text="Side View")
 
         row = layout.row()
+        row.operator("object.reset_selected_images", text="Reset Images")
+
+        row = layout.row()
         row.operator("wm.test_connection_operator", text="Test Connection")
+
+        #row = layout.row()
+        #row.operator("wm.prepare_image_operator", text="Test Image Prep")
 
 
 
@@ -222,7 +274,8 @@ class DoImg(bpy.types.Operator):
     bl_label = "Place Image"
 
     #Property that holds the filepath that will be used to insert the image
-    myFilePath: bpy.props.StringProperty(subtype="FILE_PATH")
+    myFilePath = ""
+    #bpy.props.StringProperty(subtype="FILE_PATH")
 
     #This is the function that inserts the image into blender
     def execute(self, context):
@@ -236,8 +289,6 @@ class DoImg(bpy.types.Operator):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
-
-        row.operator("wm.prepare_image_operator", text="Test Image Prep")
 
 
 class VIEW3D_PT_Sketch_To_Mesh_Align_Views_Panel(bpy.types.Panel):  
@@ -322,18 +373,16 @@ def register():
     bpy.types.Scene.Image_Center_X = bpy.props.IntProperty(name="Image Center X", default=10, min=0, max=100)
     bpy.types.Scene.Image_Center_Y = bpy.props.IntProperty(name="Image Center Y", default=10, min=0, max=100)
     bpy.types.Scene.FileName_Input = bpy.props.StringProperty(name="FileName", default="STMFile")
+    bpy.utils.register_class(Reset_Input_Images)
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_Panel)
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_Views_Panel)
     bpy.utils.register_class(PlaceImageIn3D)
     bpy.utils.register_class(DoImg)
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_Align_Views_Panel) 
     bpy.utils.register_class(VIEW3D_PT_Sketch_To_Mesh_MeshSettings_Panel)
-    # ralf changes
-    bpy.utils.register_class(StMTestConnectionOperator)
-    
    # db test connection and image prep
     bpy.utils.register_class(StMTestConnectionOperator) 
-    bpy.utils.register_class(StMTestImagePrep)  
+#    bpy.utils.register_class(StMTestImagePrep)  
 
 
 def unregister():
@@ -345,17 +394,16 @@ def unregister():
     del bpy.types.Scene.Image_Center_X
     del bpy.types.Scene.Image_Center_Y
     del bpy.types.Scene.FileName_Input
+    bpy.utils.unregister_class(Reset_Input_Images)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_Panel)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_Views_Panel)
     bpy.utils.unregister_class(PlaceImageIn3D)
     bpy.utils.unregister_class(DoImg)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_Align_Views_Panel)
     bpy.utils.unregister_class(VIEW3D_PT_Sketch_To_Mesh_MeshSettings_Panel)
-    # ralf changes
-    bpy.utils.unregister_class(StMTestConnectionOperator)
 
     # db test connection and image prep
-    bpy.utils.unregister_class(StMTestImagePrep)
+#    bpy.utils.unregister_class(StMTestImagePrep)
     bpy.utils.unregister_class(StMTestConnectionOperator)
 
 if __name__ == "__main__":
