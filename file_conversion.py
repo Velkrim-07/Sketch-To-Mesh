4e97b7f77ab20721d01a1d1a4e8c8810bc4fa71c
Velkrim-07 <burnin311@gmail.com>
2024-02-27 14:41:52 -0500

    Updated file_conversion
    Updated the file_conversion to include the .blend flies. Currently does not work with Windows11 naming scheme for the users folders, as they hyphenate the name instead of adding in the spaces. The next addition to this file will be concatenating the strings so that we can take care of this.

--- file_conversion.py
+++ file_conversion.py
@@ -1,6 +1,6 @@
+import bpy
 import io
 import os
-import base64
 import tempfile
 
 def encode_file(file_path):
@@ -10,18 +10,19 @@ def encode_file(file_path):
         return blend_file_contents
    
 def decode_file(file_path, file_extension):
-    #decode the data from base64
-    binary_data = base64.b64decode(file_path)
+    #Apparently the data doesn't need to be decoded so we will handle the different
+    #file extensions handled here instead of outside the file_conversion.py file
 
     #write the data into a temporary file
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=file_extension) #we'll probably have to add another parameter here for the file extension or soemthing else)
-    temp_file.write(binary_data)
+    temp_file.write(file_path)
     temp_file.close()
 
+    #Deal with the separate file extensions
     if file_extension == ".blend":
-        blend_opener(temp_file)
+        blend_opener(temp_file.name)
     else:
-        fbx_opener(temp_file)
+        fbx_opener(temp_file.name)
 
     #remove the temp file
     os.unlink(temp_file.name)
@@ -29,10 +30,12 @@ def decode_file(file_path, file_extension):
     #if we are returning just the file back then cases checking will have to happen outside of this method
     return 0
 
-def blend_opener(file):
+def blend_opener(file_path):
     # open up the blend file here
+    bpy.ops.wm.open_mainfile(filepath=file_path)
     return 0
 
-def fbx_opener(file):
+def fbx_opener(file_path):
     # open up the .fbx file here
+    bpy.ops.import_scene.fbx(filepath=file_path)
     return 0
